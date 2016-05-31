ccd3584de5e04db14431c9e8b306227dd987b950
fsalmoir <fsalmoir@sissa.it>
2016-05-31 16:33:14 +0200

    file handler for matlab files written

---
+++ tests/test_filehandler.py
@@ -0,0 +1,38 @@
+
+from unittest import TestCase
+import unittest
+import ezyrb.filehandler as fh
+import numpy as np
+import filecmp
+import os
+
+
+class TestFilehandler(TestCase):
+
+
+	def test_base_class_infile(self):
+		file_handler = fh.FileHandler()
+		assert file_handler.infile == None
+
+
+	def test_base_class_outfile(self):
+		file_handler = fh.FileHandler()
+		assert file_handler.outfile == None
+
+
+	def test_base_class_extension(self):
+		file_handler = fh.FileHandler()
+		assert file_handler.extension == None
+
+	
+	def test_base_class_parse(self):
+		file_handler = fh.FileHandler()
+		with self.assertRaises(NotImplementedError):
+			file_handler.parse('input')
+
+
+	def test_base_class_write(self):
+		file_handler = fh.FileHandler()
+		mesh_points = np.zeros((3, 3))
+		with self.assertRaises(NotImplementedError):
+			file_handler.write(mesh_points, 'output')
