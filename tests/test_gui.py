ac07635c8143a7d56ca6773acf054bf86e81f31f
fsalmoir <fsalmoir@sissa.it>
2016-07-14 15:37:50 +0200

    inserted the online part of the gui

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -36,6 +36,31 @@ class TestGui(TestCase):
 		assert gui_handler.url == 'https://github.com/mathLab/EZyRB'
 		
 		
+	def test_gui_init_string_7(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.tria_path.get() == ''
+		
+		
+	def test_gui_init_string_8(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.parsing_file_path.get() == ''
+		
+		
+	def test_gui_init_string_9(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.new_mu.get() == ''
+		
+		
+	def test_gui_init_string_10(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.outfilename.get() == ''
+		
+		
+	def test_gui_init_string_11(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.finish_label.get() == ''
+		
+		
 	def test_gui_init_none_1(self):
 		gui_handler = gui.Gui()
 		assert gui_handler.label_new_mu == None
@@ -63,6 +88,21 @@ class TestGui(TestCase):
 		
 	def test_gui_init_none_6(self):
 		gui_handler = gui.Gui()
+		assert gui_handler.label_tria == None
+		
+		
+	def test_gui_init_none_7(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.label_parsing_file == None
+		
+		
+	def test_gui_init_none_8(self):
+		gui_handler = gui.Gui()
+		assert gui_handler.label_finish_online == None
+		
+		
+	def test_gui_init_bool_1(self):
+		gui_handler = gui.Gui()
 		assert gui_handler.is_scalar_switch.get() == False
 		
 		
@@ -73,34 +113,5 @@ class TestGui(TestCase):
 	def test_gui_main(self):
 		interface = gui.Gui()
 		interface.main()
-	
-		
-	'''def test_gui_init_int_1(self):
-		gui_handler = gui.Gui()
-		assert gui_handler.check_var_1.get() == 0
-		
-		
-	def test_gui_init_int_2(self):
-		gui_handler = gui.Gui()
-		assert gui_handler.check_var_2.get() == 0
-		
-		
-	def test_gui_init_none_1(self):
-		gui_handler = gui.Gui()
-		assert gui_handler.label_geo == None
-		
-		
-	def test_gui_init_none_2(self):
-		gui_handler = gui.Gui()
-		assert gui_handler.label_params == None
-		
-
-	def test_gui_init_all(self):
-		gui.Gui()
-		
-		
-	def test_gui_main(self):
-		interface = gui.Gui()
-		interface.main()'''
 		
 	
