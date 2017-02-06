99f29de656f70891b7bbc63ca41751b0e0fb3e1b
Nicola Demo <demo.nicola@gmail.com>
2017-02-06 14:53:00 +0100

    Remove useless tests

--- tests/test_responsesurface.py
+++ tests/test_responsesurface.py
@@ -65,30 +65,6 @@ class TestResponseSurface(TestCase):
 		assert os.path.isfile("tests/test_datasets/ResponseSurface()space")
 		os.remove("tests/test_datasets/ResponseSurface()space")
 
-	"""
-	def test_load(self):
-		mu = Points()
-		snap = Snapshots(output_name="Pressure", dformat="point")
-		space = ResponseSurface()
-		mu.append([-.5, -.5])
-		mu.append([ .5, -.5])
-		mu.append([ .5,	 .5])
-		mu.append([-.5,	 .5])
-		snap.append("tests/test_datasets/matlab_00.vtk")
-		snap.append("tests/test_datasets/matlab_01.vtk")
-		snap.append("tests/test_datasets/matlab_02.vtk")
-		snap.append("tests/test_datasets/matlab_03.vtk")
-		space.generate(mu, snap)
-		space.save("tests/test_datasets/ResponseSurface()space")
-		another_space = ResponseSurface()
-		another_space.load("tests/test_datasets/ResponseSurface()space")
-		assert isinstance(
-				another_space.interpolator,
-				scipy.interpolate.LinearNDInterpolator
-		)
-		os.remove("tests/test_datasets/ResponseSurface()space")
-	"""
-
 	def test_loo_error(self):
 		mu = Points()
 		snap = Snapshots(output_name="Pressure", dformat="point")
@@ -103,20 +79,3 @@ class TestResponseSurface(TestCase):
 		snap.append("tests/test_datasets/matlab_03.vtk")
 		error = space.loo_error(mu, snap)
 		assert error.shape == (4, )
-
-	"""
-	def test_loo_error2(self):
-		mu = Points()
-		snap = Snapshots(output_name="Pressure", dformat="point")
-		space = ResponseSurface()
-		mu.append([-.5, -.5])
-		mu.append([ .5, -.5])
-		mu.append([ .5,	 .5])
-		mu.append([-.5,	 .5])
-		snap.append("tests/test_datasets/matlab_00.vtk")
-		snap.append("tests/test_datasets/matlab_01.vtk")
-		snap.append("tests/test_datasets/matlab_02.vtk")
-		snap.append("tests/test_datasets/matlab_03.vtk")
-		error = space.loo_error(mu, snap)
-		np.testing.assert_almost_equal(max(error), 0.149130165577, decimal=4)
-	"""
