d168287cde2c51f61f3af6f93fa466c3f7a2306a
Your Name <demo.nicola@gmail.com>
2019-10-03 14:26:09 +0200

    Add dataset for POD

--- tests/test_pod.py
+++ tests/test_pod.py
@@ -3,22 +3,21 @@ import numpy as np
 from unittest import TestCase
 from ezyrb import POD
 
-snapshots = np.load('p_snapshots.npy').T
+snapshots = np.load('tests/test_datasets/p_snapshots.npy').T
 
 class TestPOD(TestCase):
 
     def test_constructor_empty(self):
         a = POD()
-        assert False
 
     def test_1(self):
         POD('svd').reduce(snapshots)
-        assert False
+        assert False #TODO check
 
     def test_2(self):
         POD('correlation_matrix').reduce(snapshots)
-        assert False
+        assert False #TODO check
 
     def test_3(self):
         POD('randomized_svd').reduce(snapshots)
-        assert False
+        assert False #TODO check
