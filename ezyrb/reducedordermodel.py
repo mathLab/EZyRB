cd5b02220d0b2ade99680b1847789f1198d0bd68
Florian Kroetz <florian.kroetz@uni-ulm.de>
2019-10-11 14:59:24 +0200

    loo error added

--- ezyrb/reducedordermodel.py
+++ ezyrb/reducedordermodel.py
@@ -1,14 +1,14 @@
+import numpy as np
+from ezyrb import POD, RBF, Database, Scale
+
+
 class ReducedOrderModel(object):
-
     def __init__(self, database, reduction, approximation):
-
         self.database = database
         self.reduction = reduction
         self.approximation = approximation
 
-        
     def fit(self):
-        
         self.approximation.fit(
             self.database.parameters,
             self.reduction.reduce(self.database.snapshots.T))
@@ -18,4 +18,25 @@ class ReducedOrderModel(object):
     def predict(self, mu):
         print(self.approximation.predict(mu))
         return self.database.scaler_snapshots.inverse(
-                self.reduction.expand(self.approximation.predict(mu)))
+            self.reduction.expand(self.approximation.predict(mu)))
+
+    def loo_error(self, norm=np.linalg.norm):
+
+        points = self.database.parameters.shape[0]
+
+        error = np.zeros(points)
+
+        for j in np.arange(points):
+            remaining_index = list(range(j)) + list(range(j + 1, points))
+            remaining_snaps = self.database.snapshots[remaining_index]
+            remaining_param = self.database.parameters[remaining_index]
+
+            db = Database(remaining_param, remaining_snaps,
+                          scaler_snapshots=Scale('minmax'))
+            rom = ReducedOrderModel(db, self.reduction,
+                                    self.approximation).fit()
+
+            error[j] = norm(self.database.snapshots[j] -
+                                rom.predict(self.database.parameters[j]))
+
+        return error
