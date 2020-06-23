fix predict method allowing for variance return

ezyrb/gpr.py
@@ -50,16 +50,21 @@ class GPR(Approximation):
 
         self.model.optimize_restarts(optimization_restart, verbose=False)
 
-    def predict(self, new_points):
+    def predict(self, new_points, return_variance=False):
         """
         Predict the mean and the variance of Gaussian distribution at given
         `new_points`.
 
         :param array_like new_points: the coordinates of the given points.
+        :param bool return_variance: flag to return also the variance.
+            Default is False.
         :return: the mean and the variance
         :rtype: (numpy.ndarray, numpy.ndarray)
         """
-        return self.model.predict(new_points)
+        new_points = np.atleast_2d(new_points)
+        if return_variance:
+            return self.model.predict(new_points)
+        return self.model.predict(new_points)[0]
 
     def optimal_mu(self, bounds, optimization_restart=10):
         """
@@ -78,7 +83,7 @@ class GPR(Approximation):
         min_x = None
 
         def min_obj(X):
-            return -np.linalg.norm(self.predict(X.reshape(1, -1))[1])
+            return -np.linalg.norm(self.predict(X.reshape(1, -1), True)[1])
 
         initial_starts = np.random.uniform(
             bounds[:, 0],

tests/test_reducedordermodel.py
@@ -6,6 +6,7 @@ from ezyrb import ReducedOrderModel as ROM
 
 snapshots = np.load('tests/test_datasets/p_snapshots.npy').T
 pred_sol_tst = np.load('tests/test_datasets/p_predsol.npy').T
+pred_sol_gpr = np.load('tests/test_datasets/p_predsol_gpr.npy').T
 param = np.array([[-.5, -.5], [.5, -.5], [.5, .5], [-.5, .5]])
 
 
@@ -29,8 +30,8 @@ class TestReducedOrderModel(TestCase):
         gpr = GPR()
         db = Database(param, snapshots.T)
         rom = ROM(db, pod, gpr).fit()
-        pred_sol = rom.predict([-0.293344, -0.23120537]) # same with np.array([-0.293344, -0.23120537])
-        np.testing.assert_allclose(pred_sol, pred_sol_tst, rtol=1e-4, atol=1e-5)
+        pred_sol = rom.predict([-0.293344, -0.23120537])
+        np.testing.assert_allclose(pred_sol, pred_sol_gpr, rtol=1e-4, atol=1e-5)
 
     def test_loo_error(self):
         pod = POD()