7a944fd0a2f36a34156333898e9135c19821b2bf
sigvaldm <sigvaldm@fys.uio.no>
2020-12-15 10:37:50 +0100

    Bugfix #124: imports GPy before installing it

---
+++ ezyrb/meta.py
@@ -0,0 +1,9 @@
+__project__ = 'EZyRB'
+__title__ = "ezyrb"
+__author__ = "Nicola Demo, Marco Tezzele"
+__copyright__ = "Copyright 2016-2019, EZyRB contributors"
+__license__ = "MIT"
+__version__ = "1.0"
+__mail__ = 'demo.nicola@gmail.com, marcotez@gmail.com'
+__maintainer__ = __author__
+__status__ = "Stable"
