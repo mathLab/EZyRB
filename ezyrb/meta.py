8b5a08b2e8751d6cdf9aaffeba8c79c79bea61df
sigvaldm <sigvaldm@fys.uio.no>
2020-12-15 11:19:56 +0100

    Have meta.py export dunder variables

--- ezyrb/meta.py
+++ ezyrb/meta.py
@@ -1,3 +1,14 @@
+__all__ = [
+    '__project__',
+    '__title__',
+    '__author__',
+    '__copyright__',
+    '__license__',
+    '__version__',
+    '__mail__',
+    '__maintainer__',
+    '__status__']
+
 __project__ = 'EZyRB'
 __title__ = "ezyrb"
 __author__ = "Nicola Demo, Marco Tezzele"
