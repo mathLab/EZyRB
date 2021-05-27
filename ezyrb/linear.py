dd5324471c5f1eda452dee21fa54143ecc222387
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-05-27 12:14:24 +0200

    use interp1d when the first argument of Linear is 1D. tests for Linear

--- ezyrb/linear.py
+++ ezyrb/linear.py
@@ -3,7 +3,7 @@ Module for Linear N-Dimensional Interpolation
 """
 
 import numpy as np
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import LinearNDInterpolator, interp1d
 
 from .approximation import Approximation
 
@@ -27,8 +27,22 @@ class Linear(Approximation):
         :param array_like points: the coordinates of the points.
         :param array_like values: the values in the points.
         """
-        self.interpolator = LinearNDInterpolator(points, values,
-            fill_value=self.fill_value)
+        # the first dimension is the list of parameters, the second one is
+        # the dimensionality of each tuple of parameters (we look for
+        # parameters of dimensionality one)
+        as_np_array = np.array(points)
+        if not np.issubdtype(as_np_array.dtype, np.number):
+            raise ValueError('Invalid format or dimension for the argument `points`.')
+
+        if as_np_array.shape[-1] == 1:
+            as_np_array = np.squeeze(as_np_array, axis=-1)
+
+        if as_np_array.ndim == 1 or (as_np_array.ndim == 2 and
+            as_np_array.shape[1] == 1):
+            self.interpolator = interp1d(as_np_array, values, axis=0)
+        else:
+            self.interpolator = LinearNDInterpolator(points, values,
+                fill_value=self.fill_value)
 
     def predict(self, new_point):
         """
---
+++ tests/test_linear.py
@@ -0,0 +1,69 @@
+import numpy as np
+import warnings
+
+from unittest import TestCase
+from ezyrb import Linear, Database, POD, ReducedOrderModel
+
+class TestKNeighbors(TestCase):
+    def test_params(self):
+        reg = Linear(fill_value=0)
+        assert reg.fill_value == 0
+
+    def test_default_params(self):
+        reg = Linear()
+        assert np.isnan(reg.fill_value)
+
+    #def test_predict(self):
+    #    reg = Linear()
+    #    reg.fit([[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1, 0], [20, 5], [8, 6]])
+    #    result = reg.predict([[1,2], [8,9], [6,7]])
+    #    assert (result[0] == [1,0]).all()
+    #    assert (result[1] == [8,6]).all()
+    #    assert (result[2] == [20, 5]).all()
+
+    def test_predict1d(self):
+        reg = Linear()
+        reg.fit([[1], [6], [8]], [[1, 0], [20, 5], [8, 6]])
+        result = reg.predict([[1], [8], [6]])
+        assert (result[0] == [1,0]).all()
+        assert (result[1] == [8,6]).all()
+        assert (result[2] == [20, 5]).all()
+
+    def test_predict1d_2(self):
+        reg = Linear()
+        reg.fit([[1], [2]], [[1,1], [2,2]])
+        result = reg.predict([[1.5]])
+        assert (result[0] == [[1.5, 1.5]]).all()
+
+    def test_predict1d_3(self):
+        reg = Linear()
+        reg.fit([1,2], [[1,1], [2,2]])
+        result = reg.predict([[1.5]])
+        assert (result[0] == [[1.5,1.5]]).all()
+
+    def test_with_db_predict(self):
+        reg = Linear()
+        pod = POD()
+        db = Database(np.array([1, 2, 3])[:,None], np.array([1, 5, 3])[:,None])
+        rom = ReducedOrderModel(db, pod, reg)
+
+        rom.fit()
+        assert rom.predict([1]) == 1
+        assert rom.predict([2]) == 5
+        assert rom.predict([3]) == 3
+
+    def test_wrong1(self):
+        # wrong number of params
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+            with self.assertRaises(Exception):
+                reg = Linear()
+                reg.fit([[1, 2], [6,], [8, 9]], [[1, 0], [20, 5], [8, 6]])
+
+    def test_wrong2(self):
+        # wrong number of values
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+            with self.assertRaises(Exception):
+                reg = Linear()
+                reg.fit([[1, 2], [6,], [8, 9]], [[20, 5], [8, 6]])
