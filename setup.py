3690dde7d712f33233ec69da2e754b3cd9d1d911
Your Name <demo.nicola@gmail.com>
2021-11-16 10:21:01 +0100

    Update readme and setup

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-from setuptools import setup, Command
-import os
-import sys
-from shutil import rmtree
+from setuptools import setup
 
 meta = {}
 with open("ezyrb/meta.py") as fp:
@@ -26,50 +23,17 @@ EXTRAS = {
 }
 
 LDESCRIPTION = (
-    "EZyRB is a python library for the Model Order Reduction based on "
-    "baricentric triangulation for the selection of the parameter points and "
-    "on Proper Orthogonal Decomposition for the selection of the modes. It is "
-    "ideally suited for actual industrial problems, since its structure can "
-    "interact with several simulation software simply providing the output "
-    "file of the simulations."
+    "EZyRB is a Python package that performs a data-driven model order "
+    "reduction for parametrized problems exploiting the recent approaches. "
+    "Such techniques are able to provide a parametric model capable to "
+    "provide the real-time approximation of the solution of a generic "
+    "(potentially complex and non linear) problem. The reduced model is "
+    "totally built upon the numerical data obtained by the original (to "
+    "reduce) model, without requiring any knowledge of the equations that "
+    "describe this model, resulting in a well suited framework for industrial "
+    "contexts due to its natural capability to been integrated with "
+    "commercial software."
 )
-
-here = os.path.abspath(os.path.dirname(__file__))
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(VERSION))
-        os.system('git push --tags')
-
-        sys.exit()
 
 setup(
     name=NAME,
@@ -78,26 +42,20 @@ setup(
     long_description=LDESCRIPTION,
     author=AUTHOR,
     author_email=MAIL,
-	classifiers=[
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Mathematics'
-	],
-	keywords=KEYWORDS,
-	url=URL,
-	license='MIT',
-	packages=[NAME],
+    ],
+    keywords=KEYWORDS,
+    url=URL,
+    license='MIT',
+    packages=[NAME],
     install_requires=REQUIRED,
     extras_require=EXTRAS,
-    test_suite='nose.collector',
-	tests_require=['nose'],
-	include_package_data=True,
-	zip_safe=False,
-
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },)
+    include_package_data=True,
+    zip_safe=False,
+)
