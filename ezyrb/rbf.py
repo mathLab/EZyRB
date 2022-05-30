d99bc7e3153cf98c9c0efda5530fcb29d6762737
Anna Ivagnes <s274001@studenti.polito.it>
2022-05-30 16:46:49 +0200

    update RBF

--- ezyrb/rbf.py
+++ ezyrb/rbf.py
@@ -1,7 +1,7 @@
 """Module for Radial Basis Function Interpolation."""
 
 import numpy as np
-from scipy.interpolate import Rbf
+from scipy.interpolate import RBFInterpolator
 
 from .approximation import Approximation
 
@@ -15,6 +15,14 @@ class RBF(Approximation):
     :param float smooth: values greater than zero increase the smoothness of
         the approximation. 0 is for interpolation (default), the function will
         always go through the nodal points in this case.
+    :param int neighbours: if specified, the value of the interpolant at each
+        evaluation point will be computed using only this many nearest data points.
+        If None (default), all the data points are used by default.
+    :param float epsilon: Shape parameter that scales the input to the RBF.
+        If kernel is ‘linear’, ‘thin_plate_spline’, ‘cubic’, or ‘quintic’, this
+        defaults to 1 and can be ignored. Otherwise, this must be specified.
+    :param int degree: Degree of the added polynomial. The default value is
+        the minimum degree for kernel or 0 if there is no minimum degree.
 
     :cvar kernel: The radial basis function; the default is ‘multiquadric’.
     :cvar list interpolators: the RBF interpolators (the number of
@@ -33,9 +41,17 @@ class RBF(Approximation):
          >>> print(np.allclose(y, y_pred))
 
     """
-    def __init__(self, kernel='multiquadric', smooth=0):
+    def __init__(self,
+                 kernel='multiquadric',
+                 smooth=0,
+                 neighbours=None,
+                 epsilon=None,
+                 degree=None):
         self.kernel = kernel
         self.smooth = smooth
+        self.neighbours = neighbours
+        self.degree = degree
+        self.epsilon = epsilon
         self.interpolators = None
 
     def fit(self, points, values):
@@ -49,7 +65,12 @@ class RBF(Approximation):
         for value in values.T:
             argument = np.hstack([points, value.reshape(-1, 1)]).T
             self.interpolators.append(
-                Rbf(*argument, smooth=self.smooth, function=self.kernel))
+                RBFInterpolator(*argument,
+                                neighbours=self.neighbours,
+                                smoothing=self.smooth,
+                                kernel=self.kernel,
+                                epsilon=self.epsilon,
+                                degree=self.degree))
 
     def predict(self, new_point):
         """
