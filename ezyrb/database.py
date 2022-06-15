03a10ca4d5ae36fe2bda50ae53acdb11c4130bdb
MMRROOO <59205909+MMRROOO@users.noreply.github.com>
2022-06-15 09:53:32 +0200

    added spatial data to database class, and new related tests (#216)
    * added spatial data to database class, and new related tests
    * fix style issues

--- ezyrb/database.py
+++ ezyrb/database.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 
-
 class Database():
     """
     Database class
@@ -13,23 +12,35 @@ class Database():
         is None meaning no scaling.
     :param Scale scaler_snapshots: the scaler for the snapshots. Default is
         None meaning no scaling.
+    :param array_like space: the input spatial data
     """
     def __init__(self,
                  parameters=None,
                  snapshots=None,
                  scaler_parameters=None,
-                 scaler_snapshots=None):
+                 scaler_snapshots=None,
+                 space=None):
         self._parameters = None
         self._snapshots = None
+        self._space = None
         self.scaler_parameters = scaler_parameters
         self.scaler_snapshots = scaler_snapshots
 
         # if only parameters or snapshots are provided
         if (parameters is None) ^ (snapshots is None):
-            raise RuntimeError
+            raise RuntimeError(
+                'Parameters and Snapshots are not both provided')
+
+        if space is not None and snapshots is None:
+            raise RuntimeError(
+                'Snapshot data is not provided with Spatial data')
 
         if parameters is not None and snapshots is not None:
-            self.add(parameters, snapshots)
+            if space is not None:
+                self.add(parameters, snapshots, space)
+            else:
+                self.add(parameters, snapshots)
+
 
     @property
     def parameters(self):
@@ -55,6 +66,15 @@ class Database():
 
         return self._snapshots
 
+    @property
+    def space(self):
+        """
+        The matrix containing spatial information (by row).
+
+        :rtype: numpy.ndarray
+        """
+        return self._space
+
     def __getitem__(self, val):
         """
         This method returns a new Database with the selected parameters and
@@ -63,8 +83,17 @@ class Database():
         .. warning:: The new parameters and snapshots are a view of the
             original Database.
         """
-        return Database(self._parameters[val], self._snapshots[val],
-                        self.scaler_parameters, self.scaler_snapshots)
+        if self._space is None:
+            return Database(self._parameters[val],
+                            self._snapshots[val],
+                            self.scaler_parameters,
+                            self.scaler_snapshots)
+
+        return Database(self._parameters[val],
+                        self._snapshots[val],
+                        self.scaler_parameters,
+                        self.scaler_snapshots,
+                        self._space[val])
 
     def __len__(self):
         """
@@ -74,7 +103,7 @@ class Database():
         """
         return len(self._snapshots)
 
-    def add(self, parameters, snapshots):
+    def add(self, parameters, snapshots, space=None):
         """
         Add (by row) new sets of snapshots and parameters to the original
         database.
@@ -83,13 +112,31 @@ class Database():
         :param array_like snapshots: the snapshots to add.
         """
         if len(parameters) != len(snapshots):
-            raise RuntimeError('Different number of parameters and snapshots.')
+            raise RuntimeError(
+                'Different number of parameters and snapshots.')
+
+        if self._space is not None:
+            if space is None:
+                raise RuntimeError('No Spatial Value given')
+
+        if (self._space is not None) or (space is not None):
+            if space.shape != snapshots.shape:
+                raise RuntimeError(
+                    'shape of space and snapshots are different.')
+
 
         if self._parameters is None and self._snapshots is None:
             self._parameters = parameters
             self._snapshots = snapshots
+            if self._space is None:
+                self._space = space
+
+        elif self._space is None:
+            self._parameters = np.vstack([self._parameters, parameters])
+            self._snapshots = np.vstack([self._snapshots, snapshots])
         else:
             self._parameters = np.vstack([self._parameters, parameters])
             self._snapshots = np.vstack([self._snapshots, snapshots])
+            self._space = np.vstack([self._space, space])
 
         return self
