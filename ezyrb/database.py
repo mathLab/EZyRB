31b28fd0b95b0f02469264fe94437e366a6c69ad
MMRROOO <mrowkamaad@gmail.com>
2022-06-21 10:48:57 +0200

    improved visual code

--- ezyrb/database.py
+++ ezyrb/database.py
@@ -85,29 +85,34 @@ class Database():
         """
         if isinstance(val, int):
             if self._space is None:
-                return Database(np.reshape(self._parameters[val], (1,len(self._parameters[val]))),
-                                np.reshape(self._snapshots[val],(1,len(self._snapshots[val]))),
+                return Database(np.reshape(self._parameters[val],
+                                    (1,len(self._parameters[val]))),
+                                np.reshape(self._snapshots[val],
+                                    (1,len(self._snapshots[val]))),
                                 self.scaler_parameters,
                                 self.scaler_snapshots)
 
-            return Database(np.reshape(self._parameters[val], (1,len(self._parameters[val]))),
-                            np.reshape(self._snapshots[val],(1,len(self._snapshots[val]))),
+            return Database(np.reshape(self._parameters[val], 
+                                (1,len(self._parameters[val]))),
+                            np.reshape(self._snapshots[val],
+                                (1,len(self._snapshots[val]))),
                             self.scaler_parameters,
                             self.scaler_snapshots,
-                            np.reshape(self._space[val], (1,len(self._space[val]))))
+                            np.reshape(self._space[val], 
+                                (1,len(self._space[val]))))
 
-        else:
-            if self._space is None:
-                return Database(self._parameters[val],
-                                self._snapshots[val],
-                                self.scaler_parameters,
-                                self.scaler_snapshots)
-
+        
+        if self._space is None:
             return Database(self._parameters[val],
                             self._snapshots[val],
                             self.scaler_parameters,
-                            self.scaler_snapshots,
-                            self._space[val])
+                            self.scaler_snapshots)
+
+        return Database(self._parameters[val],
+                        self._snapshots[val],
+                        self.scaler_parameters,
+                        self.scaler_snapshots,
+                        self._space[val])
 
     def __len__(self):
         """
