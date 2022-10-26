4d3a2826a88090edf0e4f98757e65844cb29f3c6
Your Name <demo.nicola@gmail.com>
2022-10-26 18:03:38 +0200

    Refactor ANN and AE

--- tests/test_ae.py
+++ tests/test_ae.py
@@ -11,6 +11,10 @@ class TestAE(TestCase):
     def test_constructor_empty(self):
         AE([20, 2], [2, 20], torch.nn.ReLU, torch.nn.ReLU, 20)
 
+    def test_wrong_constructor(self):
+        with self.assertRaises(ValueError):
+            AE([20, 2], [5, 20], torch.nn.ReLU, torch.nn.ReLU, 20)
+
     def test_reconstruction(self):
         f = torch.nn.Softplus
         ae = AE([400, 20, 2], [2, 20, 400], f(), f(), 1e-5)
@@ -28,7 +32,7 @@ class TestAE(TestCase):
         assert reduced_snapshots.shape[0] == low_dim
         expanded_snapshots = ae.inverse_transform(reduced_snapshots)
         assert expanded_snapshots.shape[0] == snapshots.shape[0]
-        
+
     def test_optimizer(self):
         f = torch.nn.Softplus
         low_dim = 5
@@ -38,7 +42,7 @@ class TestAE(TestCase):
         ae = AE([200, 100, 10], [10, 100, 200], torch.nn.Tanh(), torch.nn.Tanh(), 10)
         ae.fit(snapshots)
         assert ae.optimizer == torch.optim.Adam
-    
+
     def test_optimizer_doublefit(self):
         f = torch.nn.Softplus
         low_dim = 5
