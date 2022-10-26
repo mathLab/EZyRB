c0025d4226f27c97bafcbdb850d2becfefaed1a4
Your Name <demo.nicola@gmail.com>
2022-10-26 18:03:38 +0200

    Refactor ANN and AE

--- ezyrb/ann.py
+++ ezyrb/ann.py
@@ -9,36 +9,44 @@ from .approximation import Approximation
 
 
 class ANN(Approximation):
-    def __init__(self, layers, function, stop_training, loss=None):
-        """
-        Feed-Forward Artifical Neural Network (ANN).
+    """
+    Feed-Forward Artifical Neural Network (ANN).
 
-        :param list layers: ordered list with the number of neurons of each
-            hidden layer.
-        :param torch.nn.modules.activation function: activation function at each
-            layer, except for the output layer at with Identity is considered by
-            default. A single activation function can be passed or a list of
-            them of length equal to the number of hidden layers.
-        :param list stop_training: list with the maximum number of training
-            iterations (int) and/or the desired tolerance on the training loss
-            (float).
-        :param torch.nn.Module loss: loss definition (Mean Squared if not
-            given).
+    :param list layers: ordered list with the number of neurons of each hidden
+        layer.
+    :param torch.nn.modules.activation function: activation function at each
+        layer, except for the output layer at with Identity is considered by
+        default. A single activation function can be passed or a list of them
+        of length equal to the number of hidden layers.
+    :param list stop_training: list with the maximum number of training
+        iterations (int) and/or the desired tolerance on the training loss
+        (float).
+    :param torch.nn.Module loss: loss definition (Mean Squared if not given).
+    :param torch.optim optimizer: the torch class implementing optimizer.
+        Default value is `Adam` optimizer.
+    :param float lr: the learning rate. Default is 0.001.
+    :param float l2_regularization: the L2 regularization coefficient, it
+        corresponds to the "weight_decay". Default is 0 (no regularization).
+    :param int frequency_print: the frequency in terms of epochs of the print
+        during the training of the network.
 
-        :Example:
-            >>> import ezyrb
-            >>> import numpy as np
-            >>> import torch.nn as nn
-            >>> x = np.random.uniform(-1, 1, size =(4, 2))
-            >>> y = np.array([np.sin(x[:, 0]), np.cos(x[:, 1]**3)]).T
-            >>> ann = ezyrb.ANN([10, 5], nn.Tanh(), [20000,1e-5])
-            >>> ann.fit(x, y)
-            >>> y_pred = ann.predict(x)
-            >>> print(y)
-            >>> print(y_pred)
-            >>> print(len(ann.loss_trend))
-            >>> print(ann.loss_trend[-1])
-        """
+    :Example:
+        >>> import ezyrb
+        >>> import numpy as np
+        >>> import torch.nn as nn
+        >>> x = np.random.uniform(-1, 1, size =(4, 2))
+        >>> y = np.array([np.sin(x[:, 0]), np.cos(x[:, 1]**3)]).T
+        >>> ann = ezyrb.ANN([10, 5], nn.Tanh(), [20000,1e-5])
+        >>> ann.fit(x, y)
+        >>> y_pred = ann.predict(x)
+        >>> print(y)
+        >>> print(y_pred)
+        >>> print(len(ann.loss_trend))
+        >>> print(ann.loss_trend[-1])
+    """
+    def __init__(self, layers, function, stop_training, loss=None,
+                 optimizer=torch.optim.Adam, lr=0.001, l2_regularization=0,
+                 frequency_print=10):
         if loss is None:
             loss = torch.nn.MSELoss()
 
@@ -55,7 +63,11 @@ class ANN(Approximation):
 
         self.loss_trend = []
         self.model = None
-        self.optimizer = None
+        self.optimizer = optimizer
+
+        self.frequency_print = frequency_print
+        self.lr = lr
+        self.l2_regularization = l2_regularization
 
     def _convert_numpy_to_torch(self, array):
         """
@@ -77,6 +89,19 @@ class ANN(Approximation):
         """
         return tensor.detach().numpy()
 
+    @staticmethod
+    def _list_to_sequential(layers, functions):
+
+        layers_torch = []
+        for i in range(len(layers) - 2):
+
+            layers_torch.append(nn.Linear(layers[i], layers[i + 1]))
+
+            if functions[i]:
+                layers_torch.append(functions[i])
+        layers_torch.append(nn.Linear(layers[-2], layers[-1]))
+        return nn.Sequential(*layers_torch)
+
     def _build_model(self, points, values):
         """
         Build the torch model.
@@ -92,12 +117,8 @@ class ANN(Approximation):
         layers = self.layers.copy()
         layers.insert(0, points.shape[1])
         layers.append(values.shape[1])
-        layers_torch = []
-        for i in range(len(layers) - 2):
-            layers_torch.append(nn.Linear(layers[i], layers[i + 1]))
-            layers_torch.append(self.function[i])
-        layers_torch.append(nn.Linear(layers[-2], layers[-1]))
-        self.model = nn.Sequential(*layers_torch)
+
+        self.model = self._list_to_sequential(layers, self.function)
 
     def fit(self, points, values):
         """
@@ -108,8 +129,8 @@ class ANN(Approximation):
                https://pytorch.org/docs/stable/optim.html);
             -  loss: self.loss (if none, the Mean Squared Loss is set by
                default).
-            -  stopping criterion: the fulfillment of the requested tolerance on
-               the training loss compatibly with the prescribed budget of
+            -  stopping criterion: the fulfillment of the requested tolerance
+               on the training loss compatibly with the prescribed budget of
                training iterations (if type(self.stop_training) is list); if
                type(self.stop_training) is int or type(self.stop_training) is
                float, only the number of maximum iterations or the accuracy
@@ -122,7 +143,9 @@ class ANN(Approximation):
         """
 
         self._build_model(points, values)
-        self.optimizer = torch.optim.Adam(self.model.parameters())
+        optimizer = self.optimizer(
+            self.model.parameters(),
+            lr=self.lr, weight_decay=self.l2_regularization)
 
         points = self._convert_numpy_to_torch(points)
         values = self._convert_numpy_to_torch(values)
@@ -131,20 +154,31 @@ class ANN(Approximation):
         flag = True
         while flag:
             y_pred = self.model(points)
+
             loss = self.loss(y_pred, values)
-            self.optimizer.zero_grad()
+
+            optimizer.zero_grad()
             loss.backward()
-            self.optimizer.step()
-            self.loss_trend.append(loss.item())
+            optimizer.step()
+
+            scalar_loss = loss.item()
+            self.loss_trend.append(scalar_loss)
+
             for criteria in self.stop_training:
                 if isinstance(criteria, int):  # stop criteria is an integer
                     if n_epoch == criteria:
                         flag = False
                 elif isinstance(criteria, float):  # stop criteria is float
-                    if loss.item() < criteria:
+                    if scalar_loss < criteria:
                         flag = False
 
+            if (flag is False or
+                    n_epoch == 1 or n_epoch % self.frequency_print == 0):
+                print(f'[epoch {n_epoch:6d}]\t{scalar_loss:e}')
+
             n_epoch += 1
+
+        return optimizer
 
     def predict(self, new_point):
         """
