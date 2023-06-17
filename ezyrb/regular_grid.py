8df746976d5ed47fb23616e1ee4857b8e648a4ee
Florian Arbes <florian.arbes@t-online.de>
2023-06-17 17:27:08 +0200

    minor code style improvements

--- ezyrb/regular_grid.py
+++ ezyrb/regular_grid.py
@@ -9,6 +9,7 @@ from .approximation import Approximation
 class RegularGrid(Approximation):
     """
     Multidimensional interpolator on regular grids.
+    See scipy's RegularGridInterpolator
 
     :param float fill_value: value used to fill in for requested points outside
         of the convex hull of the input points. If not provided, then the
@@ -18,14 +19,30 @@ class RegularGrid(Approximation):
     def __init__(self, fill_value=np.nan):
         self.fill_value = fill_value
         self.interpolator = None
-        return
+        self.dim = None
+        self.mode_nr = None
 
-    def fit(self, grid, values, **kvargs):
+    def fit(self, points, values, **kvargs):
         """
-        Construct the interpolator given `grid` and `values`.
+        Construct the interpolator given `points` and `values`.
 
-        :param array_like points: the coordinates of the points.
-        :param array_like values: the values in the points.
+        Parameters
+        ----------
+        points : tuple of ndarray of float, with shapes (m1, ), ..., (mn, )
+            The points defining the regular grid in n dimensions. The points in
+            each dimension (i.e. every elements of the points tuple) must be
+            strictly ascending or descending.
+
+        values : array_like, shape (m1, ..., mn, ...)
+            The data on the regular grid in n dimensions. Complex data can be
+            acceptable.
+        **kvargs : TYPE
+            see scipy.interpolate.RegularGridInterpolator
+
+        Returns
+        -------
+        None.
+
         """
         # we have two options
         # 1.: we could make an interpolator for every mode and its coefficients
@@ -33,14 +50,14 @@ class RegularGrid(Approximation):
         # option 1 is cleaner, but option 2 performs better
         # X = U S VT, X being shaped m, n
 
-        self.dim = len(grid)
+        self.dim = len(points)
         vals = np.asarray(values)
-        r, n = vals.T.shape  # vals = (S*VT).T
+        r = vals.T.shape[0]  # vals = (S*VT).T
         self.mode_nr = np.arange(r)
-        extended_grid = [self.mode_nr, *grid]
+        extended_grid = [self.mode_nr, *points]
         shape = [r, ]
         for i in range(self.dim):
-            shape.append(len(grid[i]))
+            shape.append(len(points[i]))
         assert np.prod(shape) == vals.size, "Values don't match grid. "\
             "Make sure to pass a grid, not a list of points!\n"\
             "HINT: did you use rom.fit()? This method does not work with a "\
@@ -49,19 +66,11 @@ class RegularGrid(Approximation):
                                                     vals.T.reshape(shape),
                                                     fill_value=self.fill_value,
                                                     **kvargs)
-        return
 
-    def predict(self, new_points):
-        """
-        Evaluate interpolator at given `new_points`.
-
-        :param array_like new_points: the coordinates of the given points.
-        :return: the interpolated values.
-        :rtype: numpy.ndarray
-        """
+    def predict(self, new_point):
         dim = self.dim
-        xi_extended = np.zeros((len(self.mode_nr), len(new_points), dim+1))
+        xi_extended = np.zeros((len(self.mode_nr), len(new_point), dim+1))
         xi_extended[:, :, 0] = self.mode_nr[:, None]
         for i in range(dim):
-            xi_extended[:, :, i+1] = np.array(new_points)[:, i]
+            xi_extended[:, :, i+1] = np.array(new_point)[:, i]
         return self.interpolator(xi_extended).T
